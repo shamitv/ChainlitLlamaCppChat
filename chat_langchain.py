1943bb30837f8b92646b52bdefd953b20a10ba75
Shamit Verma <github@shamit.in>
2024-07-14 15:10:11 +0530

    Added History for Chat

--- chat_langchain.py
+++ chat_langchain.py
@@ -1,42 +1,72 @@
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable
-from langchain.schema.runnable.config import RunnableConfig
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 import chainlit as cl
 
+llama_cpp_host: str = 'localhost'
+llama_cpp_port: int = 8000
+
+openai_url: str = "http://{host}:{port}/v1".format(host=llama_cpp_host, port=llama_cpp_port)
+
+# This Dictionary will store chat history in memory.
+# Key for each history object is a unique session ID
+
+history_store = {}
+
+
+def get_history(session_id: str) -> ChatMessageHistory:
+    if session_id in history_store:
+        return history_store[session_id]
+    else:
+        history = ChatMessageHistory()
+        history_store[session_id] = history
+        return history
+
+
 @cl.on_chat_start
 async def on_chat_start():
+    session_id = cl.user_session.get("id")
     model = ChatOpenAI(streaming=True,
                        api_key="...",
-                       base_url="http://i3tiny1:9750/v1")
+                       base_url=openai_url)
+    session_history = get_history(session_id)
     prompt = ChatPromptTemplate.from_messages(
         [
-            (
-                "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
-            ),
-            ("human", "{question}"),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{input}"),
         ]
     )
     runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+    with_message_history = RunnableWithMessageHistory(
+        runnable,
+        get_history,
+        input_messages_key="input",
+        history_messages_key="history",
+    )
+    cl.user_session.set("history", session_history)
+    cl.user_session.set("runnable", with_message_history)
+
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    runnable = cl.user_session.get("runnable")  # type: Runnable
-
-    msg = cl.Message(content="")
-
-    async for chunk in runnable.astream(
-        {"question": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    inp = message.content
+    session_id = cl.user_session.get("id")
+    with_message_history = cl.user_session.get("runnable")
+    config = {"configurable": {"session_id": session_id,
+                               'callbacks': [cl.LangchainCallbackHandler()]}, 'session_id': session_id}
+    async for chunk in with_message_history.astream(
+            {"input": inp},
+            config,
     ):
-        await msg.stream_token(chunk)
+        await message.stream_token(chunk)
 
-    await msg.send()
+    await message.send()
+
 
 if __name__ == "__main__":
     from chainlit.cli import run_chainlit
+
     run_chainlit(__file__)
